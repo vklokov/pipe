c4c372bc3b569bb8accb5cec258322b3687a47f0
Vladimir Klokov <vklokov@xometry.de>
2021-10-10 13:01:01 +0200

    Add persons spec

--- spec/pipe/person_spec.rb
+++ spec/pipe/person_spec.rb
@@ -1,51 +1,62 @@
 # frozen_string_literal: true
 
 RSpec.describe Pipe::Person do
-  context "#find" do
+  context "#entity" do
+    context "#find" do
 
-    subject { described_class.find(1) }
+      subject { described_class.find(1) }
 
-    it 'should return new person' do
-      expect(subject).to be_kind_of(Pipe::Person)
-      expect(subject.id).to eq(1)
+      it 'should return new person' do
+        expect(subject).to be_kind_of(Pipe::Person)
+        expect(subject.id).to eq(1)
+      end
+
+      it 'should raise Pipe::Exception::AttributeError with id = nil' do
+        expect { described_class.find(nil) }.
+          to raise_error(Pipe::Exception::AttributeError)
+      end
     end
 
-    it 'should raise Pipe::Exception::AttributeError with id = nil' do
-      expect { described_class.find(nil) }.
-        to raise_error(Pipe::Exception::AttributeError)
+    context "#update" do
+
+      subject { described_class.update(1, name: 'Example Person2') }
+
+      it 'should return updated person' do
+        expect(subject).to be_kind_of(Pipe::Person)
+        expect(subject.name).to eq('Example Person2')
+      end
+
+      it 'should raise Pipe::Exception::AttributeError with id = nil' do
+        expect { described_class.update(nil, foo: 'bar') }.
+          to raise_error(Pipe::Exception::AttributeError)
+      end
+    end
+
+    context "#delete" do
+
+      subject { described_class.delete(1) }
+
+      it 'should delete exisiting person' do
+        expect(subject).to be_kind_of(TrueClass)
+      end
+
+      it 'should raise Pipe::Exception::AttributeError with id = nil' do
+        expect { described_class.delete(nil) }.
+          to raise_error(Pipe::Exception::AttributeError)
+      end
     end
   end
 
-  context "#update" do
-
-    subject { described_class.update(1, name: 'Example Person2') }
-
-    it 'should return updated person' do
-      expect(subject).to be_kind_of(Pipe::Person)
-      expect(subject.name).to eq('Example Person2')
-    end
-
-    it 'should raise Pipe::Exception::AttributeError with id = nil' do
-      expect { described_class.update(nil, foo: 'bar') }.
-        to raise_error(Pipe::Exception::AttributeError)
+  context "#class_methods" do
+    context "#fields" do
+      it 'should return preson fields' do
+        expect(described_class.fields).to be_kind_of(Array)
+        expect(described_class.fields.first).to be_kind_of(Pipe::Field)
+      end
     end
   end
 
-  context "#delete" do
-
-    subject { described_class.delete(1) }
-
-    it 'should delete exisiting person' do
-      expect(subject).to be_kind_of(TrueClass)
-    end
-
-    it 'should raise Pipe::Exception::AttributeError with id = nil' do
-      expect { described_class.delete(nil) }.
-        to raise_error(Pipe::Exception::AttributeError)
-    end
-  end
-
-  context "#instance" do
+  context "#instance_methods" do
     let(:person_with_org) { described_class.find(1) }
     let(:person_without_org) { described_class.find(2) }
 

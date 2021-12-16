726ea314dead9d7463abec1ded3dca9d0187aadc
Vladimir Klokov <vklokov@xometry.de>
2021-12-16 17:24:09 +0300

    Add done attribute to activity

--- lib/pipe/activity.rb
+++ lib/pipe/activity.rb
@@ -3,9 +3,17 @@
 module Pipe
   class Activity
     class << self
-      def create(subject:, note:, org_id: nil, person_id: nil, deal_id: nil)
+      def create(subject:, note:, org_id: nil, person_id: nil, deal_id: nil, done: false)
         request  = Pipe::Routes::ROUTE_ACTIVITIES
-        response = Pipe::Client.new(request, subject: subject, note: note, org_id: org_id, person_id: person_id, deal_id: deal_id).post
+        Pipe::Client.new(
+          request,
+          subject: subject,
+          note: note,
+          org_id: org_id,
+          person_id: person_id,
+          deal_id: deal_id,
+          done: done ? 1 : 0,
+        ).post
       end
     end
   end

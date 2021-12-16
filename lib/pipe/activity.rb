4ea5bbbc1a9748096a53bdb8a34a0ecc007ffb21
Vladimir Klokov <vklokov@xometry.de>
2021-12-16 17:24:37 +0300

    remove unnecessary space

--- lib/pipe/activity.rb
+++ lib/pipe/activity.rb
@@ -4,7 +4,7 @@ module Pipe
   class Activity
     class << self
       def create(subject:, note:, org_id: nil, person_id: nil, deal_id: nil, done: false)
-        request  = Pipe::Routes::ROUTE_ACTIVITIES
+        request = Pipe::Routes::ROUTE_ACTIVITIES
         Pipe::Client.new(
           request,
           subject: subject,
